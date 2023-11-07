40bf855143d46c7c56729d59b3c518053e58ed0f
DaySet909 <78254936+DaySet909@users.noreply.github.com>
2023-11-07 18:31:59 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public class Class1
+{
+    //Time q = new Time(2023, 5, 20);
+
+
+
+
+    oldTime = new DateTime(2023, 5, 23);
+    newTime = DateTime.Now;
+
+            if (oldTime > newTime) throw new ArgumentException("Начальная дата не может быть больше конечной");
+
+    var years = newTime.Year - oldTime.Year;
+
+    var month = newTime.Month - oldTime.Month + (newTime.Day >= oldTime.Day - 1 ? 0 : -1) + ((oldTime.Day == 1 && DateTime.DaysInMonth(newTime.Year, newTime.Month) == newTime.Day) ? 1 : 0);
+            if (month <= 0) month = 0;
+
+            var days = (newTime.Day + DateTime.DaysInMonth(oldTime.Year, oldTime.Month) - oldTime.Day);// для чисел когда старое больше нового
+            if (newTime.Day > oldTime.Day)
+                days = newTime.Day - oldTime.Day;
+
+            Console.WriteLine($"Лет{years} месяцев{month} дней{days}");
+            return days;
+
+
+
+	public Time()
+	{
+        oldTime = new DateTime(2023, 5, 23);
+        newTime = DateTime.Now;
+
+        if (oldTime > newTime) throw new ArgumentException("Начальная дата не может быть больше конечной");
+
+        var years = newTime.Year - oldTime.Year;
+
+        var month = newTime.Month - oldTime.Month + (newTime.Day >= oldTime.Day - 1 ? 0 : -1) + ((oldTime.Day == 1 && DateTime.DaysInMonth(newTime.Year, newTime.Month) == newTime.Day) ? 1 : 0);
+        if (month <= 0) month = 0;
+
+        var days = (newTime.Day + DateTime.DaysInMonth(oldTime.Year, oldTime.Month) - oldTime.Day);// для чисел когда старое больше нового
+        if (newTime.Day > oldTime.Day)
+            days = newTime.Day - oldTime.Day;
+
+        Console.WriteLine($"Лет{years} месяцев{month} дней{days}");
+        return days;
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            //int i = 0;
+            //while (i < 1)
+            //{ 
+            DateTime qTime = new DateTime(2023, 3, 15);
+            //qTime = qTime.AddDays(i);
+            Time(qTime);
+            //    i++;
+            //}
+            string path = @"C:\Users\DenKy\Desktop\kurosava.txt";
+
+            using (StreamWriter writer = new StreamWriter(path, true))      //записыается новое значение и к нему прикрепляется дата
+            {
+                Console.WriteLine("Введите новое значение");
+                string q = Console.ReadLine();
+                writer.WriteLineAsync(q);
+            }
+
+            //using (StreamReader reader = new StreamReader(path))       //считываться должны старые данные и дата к ним
+            //{
+            //    string? line;
+            //    while ((line = reader.ReadLine()) != null)
+            //    {
+            //        Console.WriteLine(line);
+            //    }
+            //}
+
+            void Time(DateTime dateTime )
+                {
+                DateTime y = dateTime;
+                DateTime x = DateTime.Now;
+                var z = (y - x).Duration();
+
+                int oldCoin = 0;
+                int newCoin = 0;
+                oldCoin -= newCoin;
+                
+                Console.WriteLine($"Значение изменилось на {oldCoin} за {z.Days} дней ");
+                }
+            
+        }
+    }
+}
+//DateTime oldTime = dateTime;
+                //DateTime newTime = DateTime.Now;
+
+                //if (oldTime > newTime) throw new ArgumentException("Начальная дата не может быть больше конечной");
+
+                //var years = newTime.Year - oldTime.Year;
+
+                //var month = newTime.Month - oldTime.Month
+                // + (newTime.Day >= oldTime.Day - 1 ? 0 : -1);
+                //    //+ ((oldTime.Day == 1 && DateTime.DaysInMonth(newTime.Year, newTime.Month) == newTime.Day) ? 1 : 0);
+
+                //if (month < 0) month = 12 + month;
+
+                //var days = (newTime.Day + DateTime.DaysInMonth(oldTime.Year, oldTime.Month) - oldTime.Day);
+
+                //if (newTime.Day > oldTime.Day) days = newTime.Day - oldTime.Day; // для чисел когда старое больше нового
+
+                ////if (days == 31) month -= 1;
